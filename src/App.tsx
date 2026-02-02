e18cda515bf65daa8bae6a1e6fb348a19f260715
polak <08sip004@gmail.com>
2026-02-02 12:59:38 +0600

    skeleton created

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Shahirul Islam Polak 
+      </h1>
+    </>
+  )
+}
+
+export default App
